981a58b5a52208c9ea809eac5aa0ddd0a196010a
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-10-06 12:48:32 +0530

    Time: 3 ms (97.56%), Space: 8.4 MB (33.53%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,24 +1,12 @@
 class Solution
 {
-    public:
-
-        int bitcount(int n)
-        {
-            int count = 0;
-            while (n)
-            {
-                if ((n & 1) == 1) count++;
-                n = n >> 1;
-            }
-            return count;
-        }
+    public: 
     vector<int> countBits(int n)
     {
         int num = 0;
         vector<int> v;
-        for(int i=0;i<=n;i++){
-            int count = bitcount(i);
-            v.push_back(count);
+        for(int i=0;i<=n;i++){ 
+            v.push_back(__builtin_popcount(i));
         }
         return v;
     }
