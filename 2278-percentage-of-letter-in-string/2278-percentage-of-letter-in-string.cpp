c457170040616fc2c0244510122e1e90d0d5b168
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-10-11 22:58:46 +0530

    Time: 4 ms (30.26%), Space: 6 MB (82.87%) - LeetHub

---
+++ 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int percentageLetter(string s, char letter) {
+        int count=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]==letter){
+                count++;
+            }
+        }
+        return (count*100)/s.size();
+    }
+};
