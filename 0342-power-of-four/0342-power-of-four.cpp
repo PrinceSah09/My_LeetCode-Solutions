25c401d49728c006628eb0c59fe8a92967a226c7
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-10-12 19:11:29 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (71.39%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n<=0){
+            return false;
+        }
+        while(n!=1){
+            if(n%4!=0)return false;
+            n/=4;
+        }
+        return true;
+        
+    }
+};
+
+ 
