0ebe40ab56b610278aada842505206e0415bd39f
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-10-12 14:43:15 +0530

    Time: 38 ms (5.09%), Space: 5.9 MB (38.36%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -3,14 +3,10 @@
      public:
          int fib(int n)
          {
-             if (n == 0)
-             {
-                 return 0;
-             }
-             if (n == 1)
-             {
-                 return 1;
-             }
+             if (n == 0) return 0;
+            
+             if (n == 1) return 1;
+            
              int ans = fib(n - 1) + fib(n - 2);
              return ans;
          }
