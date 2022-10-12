f97fcb86b11630af2555dffbdae02ea54d13002a
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-10-12 20:16:46 +0530

    Time: 0 ms (100.00%), Space: 6 MB (97.22%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for(int i=0;i<s.length();i++){
+            if(s[i]>=65 && s[i]<=90){
+                s[i] = s[i]+32;
+            }
+        }
+        return s;
+    }
+};
