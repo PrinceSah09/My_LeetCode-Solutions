b38b34bb35b87374cfeed045fe487cf0e5b05e8e
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-10-13 17:59:43 +0530

    Time: 297 ms (32.11%), Space: 93.4 MB (51.10%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -6,10 +6,8 @@ class Solution
             int profit = 0, ibuy = 0;
             for (int i = 1; i < prices.size(); i++)
             {
-                if (prices[ibuy] > prices[i])
-                {
-                    ibuy = i;
-                }
+                if (prices[ibuy] > prices[i]) ibuy = i;
+              
                 profit = max(profit, prices[i] - prices[ibuy]);
             }
             return profit;
