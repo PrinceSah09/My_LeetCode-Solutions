abdc416ea770078d634c1df4751b97e7596e4187
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-10-15 20:15:50 +0530

    Time: 79 ms (21.06%), Space: 71.3 MB (91.43%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -10,14 +10,10 @@
 
              while (l1 || l2 || extra)
              {
-                 if (l1)
-                 {
-                     extra += l1->val, l1 = l1->next;
-                 }
-                 if (l2)
-                 {
-                     extra += l2->val, l2 = l2->next;
-                 }
+                 if (l1) extra += l1->val, l1 = l1->next;
+              
+                 if (l2) extra += l2->val, l2 = l2->next;
+           
                  p->next = new ListNode(extra % 10);
                  extra = extra / 10;
                  p = p->next;
