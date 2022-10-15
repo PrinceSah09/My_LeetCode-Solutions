35109b985b763b3d593bd075004551cc6e51ef2a
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-10-15 20:17:53 +0530

    Time: 72 ms (33.91%), Space: 71.3 MB (91.43%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,23 +1,19 @@
- class Solution
- {
-     public:
-         ListNode* addTwoNumbers(ListNode *l1, ListNode *l2)
-         {
-
-             ListNode headNode(0);
-             ListNode *p = &headNode;
-             int extra = 0;
-
-             while (l1 || l2 || extra)
-             {
-                 if (l1) extra += l1->val, l1 = l1->next;
-              
-                 if (l2) extra += l2->val, l2 = l2->next;
-           
-                 p->next = new ListNode(extra % 10);
-                 extra = extra / 10;
-                 p = p->next;
-             }
-             return headNode.next;
-         }
- };
+class Solution
+{
+    public:
+        ListNode* addTwoNumbers(ListNode *l1, ListNode *l2)
+        {
+            ListNode preHead(0), *p = &preHead;
+            int extra = 0;
+            while (l1 || l2 || extra)
+            {
+                int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + extra;
+                extra = sum / 10;
+                p->next = new ListNode(sum % 10);
+                p = p->next;
+                l1 = l1 ? l1->next : l1;
+                l2 = l2 ? l2->next : l2;
+            }
+            return preHead.next;
+        }
+};
