7f7f35da85ab3e5f082bb47fe8a2ec9f907230e2
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-10-16 16:14:55 +0530

    Time: 7 ms (17.92%), Space: 5.9 MB (71.27%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n<0)return false;
-        if(__builtin_popcount(n)==1)return true;
-        else return false;
+        // Base Case
+        if(n<1)return false;
+        if(n==1) return true;
+        
+        if(n%2==1)return false;
+        
+        return isPowerOfTwo( n/2);
     }
 };
