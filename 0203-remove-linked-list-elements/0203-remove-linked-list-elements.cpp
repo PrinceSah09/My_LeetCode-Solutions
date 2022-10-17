18fcfb980dc2de358c756780c09bb6fa1c47637c
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-10-17 11:57:28 +0530

    Time: 49 ms (45.31%), Space: 15 MB (80.27%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -1,31 +1,63 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* removeElements(ListNode* head, int val) {
-        while(head !=NULL && head->val==val){
-            head=head->next;
-        }
-        if(head==NULL)return head;
-        
-        ListNode*temp = head;
-        while(temp->next!=NULL){
-            if(temp->next->val==val){
-                temp->next=temp->next->next;
+class Solution
+{
+    public:
+        ListNode* removeElements(ListNode *head, int val)
+        {
+
+            /*3 -> 3-> 5 -> 3 -> 7  ||  here head->val = 3;
+
+            Step 1: we start a loop till 'head->val != val',             LL will become=> 5 -> 3 -> 7
+
+            Step 2: If head == NULL means all val of Node was equal to given
+            'val' hence we return head itself
+
+              ex: 3 -> 3-> 3-> 3-> 3   => from above loop our head currently
+              pointing to last node
+              because if(head->val==va;)head=head->next;
+              hence we return head which is currently NULL.
+
+            Step 3: For iterate through LL we create a new pointer
+                which is initially pointing to head Node
+
+            Step 4: Start a loop till 'temp->next != NULL' we just check wether
+                    the val of a 'current->next' node
+                    is equal to the given val, If yes then we just skip that Node
+                    and point our current pointer to 
+                    the current->next->next pointer 
+                    else: we go to the next pointer using temp = temp->next .
+                    And lastly we return head.
+                    why? beacuse temp is currently at the last Node and head
+                    is still at first position, in the given 
+                    qestion we have to return head of a node, In other word
+                    first pointer of a node 
+
+                 Hope this will help :)
+            */
+
+           	// Step 1:
+            while (head != NULL && head->val == val)
+            {
+                head = head->next;
             }
-            else{
-                temp = temp->next;
+
+           	// Step 2:
+            if (head == NULL) return head;
+
+           	// Step 3:
+            ListNode *temp = head;
+
+           	// Step 4:
+            while (temp->next != NULL)
+            {
+                if (temp->next->val == val)
+                {
+                    temp->next = temp->next->next;
+                }
+                else
+                {
+                    temp = temp->next;
+                }
             }
+            return head;
         }
-        return head;
-        
-    }
 };
