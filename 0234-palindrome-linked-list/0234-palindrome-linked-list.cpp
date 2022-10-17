a26ef485596ea4d0f439cd65ff932b8182f9d5cb
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-10-17 19:01:20 +0530

    Time: 543 ms (19.27%), Space: 114.2 MB (77.09%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,31 +1,123 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    
-    bool get(ListNode*head){
-        if(head==NULL)return false;
-        vector<int>v;
-        while(head!=NULL){
-            v.push_back(head->val);
-            head = head->next;
+// /**
+//  *Definition for singly-linked list.
+//  *struct ListNode {
+//  *int val;
+//  *ListNode * next;
+//  *ListNode() : val(0), next(nullptr) {}
+//  *ListNode(int x) : val(x), next(nullptr) {}
+//  *ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  *};
+//  */
+// class Solution
+// {
+//     public:
+
+//        	//Function for find middle node
+//         ListNode* getmid(ListNode *head)
+//         {
+//             ListNode *slow = head;
+//             ListNode *fast = head->next;
+
+//             while (fast != NULL && fast->next != NULL)
+//             {
+//                 fast = fast->next->next;
+//                 slow = slow->next;
+//             }
+//             return slow;
+//         }
+
+//    	//Function for reversing a linkedd list
+//     ListNode* rev(ListNode *head)
+//     {
+
+//         ListNode *prev = NULL;
+//         ListNode *curr = head;
+//         ListNode *frwd = NULL;
+
+//         while (curr != NULL)
+//         {
+//             frwd = curr->next;
+//             curr->next = prev;
+
+//             curr = frwd;
+//             prev = curr;
+//         }
+//         return prev;
+//     }
+//     bool isPalindrome(ListNode *head)
+//     {
+
+//         if (head->next == NULL) return true;
+//         if(head->next->next == NULL)return false;
+//        	//Step 1:Find Middle
+//         ListNode *middle = getmid(head);
+
+//        	//Step 2: Reverse List after middle node
+//         ListNode *temp = middle->next;
+//         middle->next = rev(temp);
+
+//         ListNode *head1 = head;
+//         ListNode *head2 = middle->next;
+
+//         while (head2  != NULL)
+//         {
+//             if (head1->val != head2->val)
+//             {
+//                 return false;
+//             }
+//             head1 = head1->next;
+//             head2 = head2->next;
+//         }
+
+//         return true;
+//     }
+// };
+
+class Solution
+{
+    public:
+        ListNode* findMid(ListNode *head)
+        {
+            ListNode *slow = head, *fast = head;
+            while (fast != NULL && fast->next != NULL)
+            {
+                slow = slow->next;
+                fast = fast->next;
+                if (fast) fast = fast->next;
+            }
+            return slow;
         }
-        int s=0,e=v.size()-1;
-        while(s<=e){
-            if(v[s++]!=v[e--])return false;
+    ListNode* reverseList(ListNode *head)
+    {
+        ListNode *prev = NULL, *curr = head, *temp = NULL;
+        while (curr != NULL)
+        {
+            temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+    
+    bool compareList(ListNode *head1, ListNode *head2)
+    {
+        while (head1 != NULL && head2 != NULL)
+        {
+            if (head1->val != head2->val) return false;
+            head1 = head1->next;
+            head2 = head2->next;
         }
         return true;
     }
     
-    bool isPalindrome(ListNode* head) {
-        return get(head);
+    
+    bool isPalindrome(ListNode *head)
+    {
+        if (head == NULL || head->next == NULL) return true;
+        ListNode *h = head;
+        ListNode *mid = findMid(h);
+        ListNode *h2 = reverseList(mid);
+        return compareList(head, h2);
     }
 };
