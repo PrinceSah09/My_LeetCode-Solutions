15dc8ebb61578747ed7a091c64d96f2505f34ba0
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-10-18 13:26:54 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (92.71%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -1,35 +1,47 @@
 class Solution
 {
+   	//     public:
+
+   	//         int get(string s)
+   	//         {
+   	//             int dec = 0, base = 1;
+   	//             int size = (int) s.length();
+
+   	//             for (int i = size - 1; i >= 0; i--)
+   	//             {
+   	//                 if (s[i] == '1')
+   	//                 {
+   	//                     dec += base;
+   	//                 }
+   	//                 base *= 2;
+   	//             }
+
+   	//             return dec;
+   	//         }
+
+   	//     int getDecimalValue(ListNode *head)
+   	//     {
+   	//         string s;
+   	//         while (head != NULL)
+   	//         {
+   	//             int data = head->val;
+   	//             char cand = (data == 1) ? '1' : '0';
+   	//             s += cand;
+   	//             head = head->next;
+   	//         }
+   	//         int ans = get(s);
+   	//         return ans;
+   	//     }
+
     public:
-
-        int get(string s)
+        int getDecimalValue(ListNode *head)
         {
-            int dec = 0, base = 1;
-            int size = (int) s.length();
-
-            for (int i = size - 1; i >= 0; i--)
+            int result = head->val;
+            while (head->next)
             {
-                if (s[i] == '1')
-                {
-                    dec += base;
-                }
-                base *= 2;
+                result = result *2 + head->next->val;
+                head = head->next;
             }
-
-            return dec;
+            return result;
         }
-
-    int getDecimalValue(ListNode *head)
-    {
-        string s;
-        while (head != NULL)
-        {
-            int data = head->val;
-            char cand = (data == 1) ? '1' : '0';
-            s += cand;
-            head = head->next;
-        }
-        int ans = get(s);
-        return ans;
-    }
 };
