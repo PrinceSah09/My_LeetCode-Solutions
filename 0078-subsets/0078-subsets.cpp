38a59915820f38fcf429fcd706679fcb72be50f9
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-10-18 15:35:34 +0530

    Time: 10 ms (18.49%), Space: 16.8 MB (9.15%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,32 +1,36 @@
-class Solution
-{
-    public:
-        void solve(vector<int> nums, vector<vector< int >> &ans, int index, vector< int > output)
-        {
-           	// bse case
-            if (index >= nums.size())
-            {
-                ans.push_back(output);
-                return;
-            }
-
-           	//exclude
-            solve(nums, ans, index + 1, output);
-
-           	//include
-            int element = nums[index];
-            output.push_back(element);
-            solve(nums, ans, index + 1,output);
+class Solution {
+public:
+    
+    
+    void solve(vector<int>nums,vector<vector<int>>&badka, vector<int>chotka, int index ){
+        //Base Case
+        if(index>=nums.size()){
+            //badka vector me chotka dal denaa
+            badka.push_back(chotka);
+            return;
         }
-
-    vector<vector < int>> subsets(vector<int> &nums)
-    {
-        vector<vector < int>> ans;
-        vector<int> output;
-        int index = 0;
-
-        solve(nums, ans, index, output);
-        return ans;
         
+        //Exclude
+        solve(nums,badka,chotka,index+1);
+        
+        //Include
+        int cand = nums[index];
+        chotka.push_back(cand);
+        solve(nums,badka,chotka, index+1);
+        
+    }
+    
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        // create badka vector
+        vector<vector<int>>badka;
+        
+        //create chotka for operation
+        vector<int>chotka;
+        
+        //make recursive call
+        int index = 0;
+        solve(nums,badka,chotka,index);
+        return badka;
     }
 };
