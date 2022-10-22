aa69655289bc02a1b443b83d2bd5c9e2832fc049
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-10-22 23:32:48 +0530

    Time: 359 ms (5.08%), Space: 54.6 MB (16.38%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,14 +3,15 @@
      public:
          bool containsDuplicate(vector<int> &nums)
          {
-             sort(nums.begin(), nums.end());
-             for (int i = 0; i < nums.size() - 1; i++)
-             {
-                 if (nums[i] == nums[i + 1])
-                 {
-                     return true;
-                 }
+             map<int,int>m;
+             for(int i=0;i<nums.size();i++){
+                 m[nums[i]]++;
              }
+             
+             for(int i=0;i<nums.size();i++){
+                 if(m[nums[i]]>=2)return true;
+             }
+             
              return false;
          }
  };
