87f962c5b14bc27745e25ec41a87bd82450598d4
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-10-22 19:51:33 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (67.31%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.cpp
+++ 1952-three-divisors/1952-three-divisors.cpp
@@ -1,31 +1,33 @@
+// class Solution
+// {
+//     public:
+//         bool isThree(int n)
+//         {
+//             int count = 0;
+//             for (int i = 1; i <= n; i++)
+//             {
+//                 if (n % i == 0) count++;
+//             }
+//             return 1 ? (count == 3) : 0;
+//         }
+
+// };
+
 class Solution
 {
+
     public:
         bool isThree(int n)
         {
             int count = 0;
             for (int i = 1; i <= n; i++)
             {
-                if (n % i == 0) count++;
+                if (n % i == 0)
+                    count++;
+               	//there is no need to count more than 4    
+                if (count > 3)
+                    break;
             }
-            return 1 ? (count == 3) : 0;
+            return count == 3;
         }
-        
 };
-
-/* 
-bool isThree(int n)
-{
-    int count = 0;
-    for (int i = 1; i <= n; i++)
-    {
-        if (n % i == 0)
-            count++;
-        //there is no need to count more than 4    
-        if(count>3)
-        break;
-    }
-    return count == 3;
-}
-
-*/
