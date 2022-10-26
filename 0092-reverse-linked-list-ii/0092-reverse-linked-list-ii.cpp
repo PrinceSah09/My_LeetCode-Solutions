6f2a34ef37c61ddbf7ab2a645ccbf9de61c1f495
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-10-26 23:46:40 +0530

    Time: 3 ms (78.91%), Space: 7.5 MB (19.09%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -29,40 +29,61 @@
             	//              }
 
             	//              return dummy.next;
-            	//          }
+            	//   
+                // }
+         
+         // **********************************************************************
 
-             if (head == NULL) return NULL;
+//              if (head == NULL) return NULL;
 
-             ListNode *cur = head, *prev = NULL;
-             while (left > 1)
-             {
-                 prev = cur;
-                 cur = cur->next;
-                 left--;
-                 right--;
-             }
+//              ListNode *cur = head, *prev = NULL;
+//              while (left > 1)
+//              {
+//                  prev = cur;
+//                  cur = cur->next;
+//                  left--;
+//                  right--;
+//              }
 
-             ListNode *start = prev, *tail = cur;
-             ListNode *temp = NULL;
-             while (right > 0)
-             {
-                 temp = cur->next;
-                 cur->next = prev;
-                 prev = cur;
-                 cur = temp;
-                 right--;
-             }
+//              ListNode *start = prev, *tail = cur;
+//              ListNode *temp = NULL;
+//              while (right > 0)
+//              {
+//                  temp = cur->next;
+//                  cur->next = prev;
+//                  prev = cur;
+//                  cur = temp;
+//                  right--;
+//              }
 
-             if (start != NULL)
-             {
-                 start->next = prev;
-             }
-             else
-             {
-                 head = prev;
-             }
-             tail->next = cur;
-             return head;
+//              if (start != NULL)
+//              {
+//                  start->next = prev;
+//              }
+//              else
+//              {
+//                  head = prev;
+//              }
+//              tail->next = cur;
+//              return head;
+             
+  // **********************************************************************           
+             
+             
+              
+            ListNode *dummy = new ListNode(0), *pre = dummy, *cur;
+            dummy->next = head;
+            for (int i = 0; i < left - 1; i++){
+                pre = pre->next;
+            }
+            cur = pre->next;
+            for (int i = 0; i < right - left; i++){
+                ListNode *temp = pre->next;
+                pre->next = cur->next;
+                cur->next = cur->next->next;
+                pre->next->next = temp;
+            }
+            return dummy->next; 
          }
  };
 
