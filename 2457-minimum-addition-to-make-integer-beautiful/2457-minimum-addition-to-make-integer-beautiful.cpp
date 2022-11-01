e0a33779bba8d1dcd93e01bedc5136b55107e1fb
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-11-01 13:45:38 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (85.28%) - LeetHub

--- 2457-minimum-addition-to-make-integer-beautiful/2457-minimum-addition-to-make-integer-beautiful.cpp
+++ 2457-minimum-addition-to-make-integer-beautiful/2457-minimum-addition-to-make-integer-beautiful.cpp
@@ -2,24 +2,24 @@ class Solution
 {
     public:
 
-        long long digsum(long long n)
+        long long digitsum(long long n)
         {
             long long sum = 0;
             while (n)
             {
-                sum += n % 10;
-                n /= 10;
+                sum = sum + (n % 10);
+                n = n / 10;
             }
             return sum;
         }
 
     long long makeIntegerBeautiful(long long n, int target)
     {
-        if (digsum(n) <= target) return 0;
+        if (digitsum(n) <= target) return 0;
+        
         long long count = 10;
         long long temp = n;
-
-        while (digsum(temp) > target)
+        while (digitsum(temp) > target)
         {
             long long mod = n % count;
             temp = n + count - mod;
