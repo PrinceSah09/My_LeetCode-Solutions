6a6192808b981c611319396e155da8f0a217e6ac
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-11-04 08:30:45 +0530

    Time: 20 ms (98.78%), Space: 19.7 MB (83.21%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.cpp
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.cpp
@@ -3,15 +3,48 @@ class Solution
     public:
         int findMaxK(vector<int> &nums)
         {
-            int ans = -1;
+           	// sort the numbers O(nlogn)
             sort(nums.begin(), nums.end());
-            for (int i = 0; i < nums.size(); i++)
+            int l = 0, h = nums.size() - 1;
+            while (l < h)
             {
-                for (int j = i+1; j < nums.size(); j++)
+                int sum = nums[l] + nums[h];
+               	// if sum is zero we have found our answer
+                if (sum == 0)
+                    return nums[h]; 
+                else if (sum > 0)
                 {
-                    if (nums[i] != 0 && nums[i] == -(nums[j]))  ans = max(ans, nums[j]); 
+                    h--;
                 }
-            }
-            return ans;
+                else
+                {
+                    l++;
+                }
+            } 
+            return -1;
         }
-};
+    
+};   //  => TC (Nlog N) 
+   
+
+ 
+
+   	// class Solution
+   	// {
+   	//     public:
+   	//         int findMaxK(vector<int> &nums)
+   	//         {
+   	//             int ans = -1;
+   	//             sort(nums.begin(), nums.end());
+   	//             for (int i = 0; i < nums.size(); i++)
+   	//             {
+   	//                 for (int j = i+1; j < nums.size(); j++)
+   	//                 {
+   	//                     if (nums[i] != 0 && nums[i] == -(nums[j]))  ans = max(ans, nums[j]); 
+   	//                 }
+   	//             }
+   	//             return ans;
+   	//         }
+   	// };
+
+     //  => TC (N)^2        
