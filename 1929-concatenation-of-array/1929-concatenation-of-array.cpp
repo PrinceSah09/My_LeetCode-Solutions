017e17d92139025b7280b81e8373e5a116c28d47
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-11-04 10:01:43 +0530

    Time: 21 ms (40.98%), Space: 12.8 MB (53.56%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -5,9 +5,8 @@ class Solution
         {
             
             int size = nums.size();
-            for(int i=0;i<size;i++){
-                nums.push_back(nums[i]);
-            }
+            for(int i=0;i<size;i++) nums.push_back(nums[i]);
+          
             return nums;
             // vector<int> ans;
             // for (int it: nums) ans.push_back(it);
