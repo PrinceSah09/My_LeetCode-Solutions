983b1d67d14ac276086756ce78b025adc6a18c01
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-11-05 09:17:20 +0530

    Time: 14 ms (34.90%), Space: 9.8 MB (27.03%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,17 +1,13 @@
-class Solution {
-public:
-    vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int>m;
-        vector<int>j;
-        vector<int>ans;
-        for(int i=0;i<n;i++)m.push_back(nums[i]);
-        for(int i=n;i<(2*n);i++)j.push_back(nums[i]);
-        int x=0,y=0;
-        while(!m.empty()){
-            ans.push_back(m[x++]);
-            ans.push_back(j[y++]);
-            m.pop_back();
+class Solution
+{
+    public:
+        vector<int> shuffle(vector<int> &nums, int n)
+        {
+            vector<int> ans;
+            for(int i=0;i<n;i++){
+                ans.push_back(nums[i]);
+                ans.push_back(nums[n+i]);
+            }
+            return ans;
         }
-        return ans;
-    }
 };
