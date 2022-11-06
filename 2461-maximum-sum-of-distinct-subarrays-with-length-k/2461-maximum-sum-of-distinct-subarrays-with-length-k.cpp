3b32db00ad51d9d07edd15344fa59f526ca90cb4
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-11-06 10:44:43 +0530

    Time: 647 ms (12.50%), Space: 97.4 MB (12.50%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,53 +1,57 @@
-#define ll long long  
-#define umii unordered_map<int,int>  
-  
-class Solution {  
-public:  
-    long long maximumSubarraySum(vector<int>& nums, int k) {  
-        ll i=0;  
-        unordered_map<ll,ll>mp;  
-        ll sum=0;  
-        while(i<k-1){  
-            mp[nums[i]]++;  
-            sum+=nums[i];  
-            i++;  
-              
-        }  
-        ll j=0;  
-        ll ans= LLONG_MIN;  
-        while(i<nums.size()){  
-            mp[nums[i]]++;  
-            sum+=nums[i];  
-            if(mp.size()==k){  
-                ans= max(ans, sum);  
-            }  
-            sum-=nums[j];  
-            if(mp[nums[j]]==1) mp.erase(nums[j]);  
-            else mp[nums[j]]--;  
-            j++;  
-            i++;  
-        }  
-        if(ans==LLONG_MIN) return 0;  
-        return ans;  
-    }  
-};
+
+#define ll long long
+#define umii unordered_map<int, int>
+
+    class Solution
+    {
+        public:
+            long long maximumSubarraySum(vector<int> &nums, int k)
+            {
+                ll i = 0;
+                unordered_map<ll, ll> mp;
+                ll sum = 0;
+                while (i < k - 1)
+                {
+                    mp[nums[i]]++;
+                    sum += nums[i];
+                    i++;
+                }
+                ll j = 0;
+                ll ans = LLONG_MIN;
+                while (i < nums.size())
+                {
+                    mp[nums[i]]++;
+                    sum += nums[i];
+                    if (mp.size() == k)
+                    {
+                        ans = max(ans, sum);
+                    }
+                    sum -= nums[j];
+                    if (mp[nums[j]] == 1) mp.erase(nums[j]);
+                    else mp[nums[j]]--;
+                    j++;
+                    i++;
+                }
+                if (ans == LLONG_MIN) return 0;
+                return ans;
+            }
+    };
 
 // class Solution
 // {
 //     public:
 
-//         // [1,5,4,2,9,9,9], k = 3
-    
-//         // => 
+//        	//[1,5,4,2,9,9,9], k = 3
+
+//        	// => 
 //     bool get(vector<int> &nums, int start ,int count){
 //         while(count--){
 //             if(nums[start] == nums[start+1])return false;
-           
+
 //             start++;
 //         }
 //         return true;
 //     }
- 
 
 //     long long maximumSubarraySum(vector<int> &nums, int k)
 //     {
@@ -67,7 +71,7 @@ public:
 //                 }
 //                  maxi = max(sum, maxi); 
 //             }
-               
+
 //         }
 //         return maxi;
 //     }
