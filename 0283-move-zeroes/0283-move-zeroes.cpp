4f22653f1474035f39885cb333e7b20e30a17dba
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-11-08 09:11:23 +0530

    Time: 29 ms (85.63%), Space: 19.3 MB (58.01%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,11 +1,13 @@
-class Solution {
-public:
-    void moveZeroes(vector<int>& nums) {
-        int x=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]!=0){
-                swap(nums[i],nums[x++]);
+class Solution
+{
+    public:
+        void moveZeroes(vector<int> &nums)
+        {
+            int x = 0;
+            for (int i = 0; i < nums.size(); i++)
+            {
+                if (nums[i] != 0) swap(nums[i], nums[x++]);
             }
         }
-    }
 };
+    
