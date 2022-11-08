90d22d8b018bbb9f6a24ca5b361e0646121995a2
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-11-09 01:20:34 +0530

    Time: 7 ms (40.76%), Space: 6.1 MB (97.68%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        // Use stack to store the visited characters.
+        vector<char> v;
+        
+        // Iterate over 's'.
+        for (auto currChar : s) {
+            // If the current character make a pair with the last character in the stack,
+            // remove both of them. Otherwise, we add the current character to stack.
+            if (!v.empty() && abs(v.back() - currChar) == 32)
+                v.pop_back(); 
+            else
+                v.push_back(currChar);
+        }
+        
+        // Returns the string concatenated by all characters left in the stack.
+        string ans(v.begin(), v.end());
+        return ans;
+    }
+};
