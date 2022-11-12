8f517e6ab16abc65d373777098efadf612a4ca95
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-11-12 19:51:21 +0530

    Time: 435 ms (15.59%), Space: 59.3 MB (95.15%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.cpp
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.cpp
@@ -2,20 +2,33 @@ class Solution{
     public:
         int nearestValidPoint(int x, int y, vector<vector < int>> &points)
         {
+            
+            // create a index and assign i
             int index = -1;
+            
+            // create a index and assign i
             int min = INT_MAX;
+            
+            // Traverse the 2D array (We dont need to traverse the column as there is only two points {0,1}) 
             for (int i = 0; i < points.size(); i++)
             {
+                
+                //Find the valid point by checking current distance point with the given point (either x should be present in the point or y should be present)
                     if( x==points[i][0] || y == points[i][1]){
+                        
+                        // Calculate the manhattan distance and store it in the current distance
                     int curr = abs(x - points[i][0]) + abs(y - points[i][1]);
+                        
+                        // Check the min distance if it is smaller , then update the min distance with current distance and as we know we have to return the index so update the index also with the current index
                     if (curr < min)
                     { 
-                        min = curr;
+                        min = curr; 
                         index = i;
                     } 
                  }
-            }
+            } 
             
+            // Then return the index
             return index;
         } 
 }; 
