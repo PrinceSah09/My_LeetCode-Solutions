3bd9b355f3f042dca0990235bdd1a639551c9458
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-11-14 17:03:20 +0530

    Time: 4 ms (44.77%), Space: 6 MB (97.58%) - LeetHub

---
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        if(s1==s2)
+            return true;
+        int count=0;
+        for(int i=0;i<s1.length();i++){
+            if(s1[i]!=s2[i])
+                count++;
+        }
+        
+        if(count<=2){
+            sort(s1.begin(),s1.end());
+            sort(s2.begin(),s2.end());
+            if(s1==s2)
+                return true;
+        }
+        return false;
+    }
+};
