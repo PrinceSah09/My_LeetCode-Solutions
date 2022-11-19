251ab892d4fac37c9fe4659fb915c4dbcb02369a
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-11-19 08:59:22 +0530

    Time: 15 ms (21.11%), Space: 7.9 MB (51.64%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -1,16 +1,13 @@
-class Solution
-{
-    public:
-        int maximumWealth(vector<vector < int>> &accounts)
-        {
-            int mx = INT_MIN;
-            int size = accounts.size();
-            for (int i = 0; i < size; i++)
-            {
-                int sum = 0;
-                for (int j = 0; j < accounts[i].size(); j++) sum += accounts[i][j];
-                mx = max(sum, mx);
-            }
-            return mx;
+ class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int mx=INT_MIN;
+        int size =  accounts.size();
+        for(int i=0;i<size;i++){
+        int sum=0;
+            for(int j=0;j<accounts[i].size();j++) sum+=accounts[i][j];
+            mx=max(sum,mx); 
         }
+        return mx;
+    }
 };
