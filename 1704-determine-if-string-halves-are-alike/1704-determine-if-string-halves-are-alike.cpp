a85a9a07c1542f98537e49d9fea120466d2b79b5
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-12-01 20:13:17 +0530

    Time: 4 ms (73.06%), Space: 6.6 MB (56.35%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -2,26 +2,36 @@ class Solution
 {
     public:
 
+       	// Function for checking wether given character is vowel or not.
+
         bool isvowel(char x)
         {
             if (x == 'a' || x == 'i' || x == 'e' || x == 'o' || x == 'u' ||
                 x == 'A' || x == 'I' || x == 'E' || x == 'O' || x == 'U') return true;
             return false;
         }
-    
+
     bool halvesAreAlike(string s)
     {
+       	//Creating some variable for simplicity :)
 
+       	// n for size of string
+       	// v1 for count vowel in first half string
+       	// v2 for count vowel in second half string
         int n = s.length(), v1 = 0, v2 = 0;
-        for (int i = 0, j = (n / 2) ; i < (n / 2) - 1, j < n; i++, j++)
+
+        /* In this for loop we have two pointer i and j;
+        Lets we have ("AbCdEfGh") size = 8, i starting from 0th index and ending at leass then size/2 (not<=) and 
+        j pointer is starting from n/2 (8/2 = 4th index ('E') and ending at string of size 
+        */
+        for (int i = 0, j = (n / 2); i < (n / 2) - 1, j < n; i++, j++)
         {
+           	// if the input char is vowel then we increment value of v1 and v2 
             if (isvowel(s[i])) v1++;
             if (isvowel(s[j])) v2++;
         }
         
-        // "AbCd EfGh"
-        //  0    4
-        
+        // if v1 is equal to v2 then return true else return false
         if (v1 == v2) return true;
         return false;
     }
