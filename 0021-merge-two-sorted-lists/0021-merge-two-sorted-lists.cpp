d082c177dbdcf8394a4ba25924d4c6b10c4dd501
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-12-03 20:49:10 +0530

    Time: 20 ms (21.87%), Space: 14.9 MB (42.71%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -1,36 +1,26 @@
- class Solution
- {
-     public:
-         ListNode* mergeTwoLists(ListNode *l1, ListNode *l2)
-         {
-             {
-                	// if list1 happen to be NULL
-                	// we will simply return list2.
-                 if (l1 == NULL)
-                 {
-                     return l2;
-                 }
+ListNode* get(ListNode *list1, ListNode *list2)
+{
 
-                	// if list2 happen to be NULL
-                	// we will simply return list1.
-                 if (l2 == NULL)
-                 {
-                     return l1;
-                 }
+   	//through recursion 
+    if (list1 == NULL) return list2;
+    if (list2 == NULL) return list1;
 
-                	// if value pointend by l1 pointer is less than equal to value pointed by l2 pointer
-                	// we wall call recursively l1 -> next and whole l2 list.
-                 if (l1->val <= l2->val)
-                 {
-                     l1->next = mergeTwoLists(l1->next, l2);
-                     return l1;
-                 }
-                	// we will call recursive l1 whole list and l2 -> next
-                 else
-                 {
-                     l2->next = mergeTwoLists(l1, l2->next);
-                     return l2;
-                 }
-             }
-         }
- };
+    if (list1->val < list2->val)
+    {
+        list1->next = get(list1->next, list2);
+        return list1;
+    }
+    else
+    {
+        list2->next = get(list1, list2->next);
+        return list2;
+    }
+}
+class Solution
+{
+    public:
+        ListNode* mergeTwoLists(ListNode *list1, ListNode *list2)
+        {
+            return get(list1, list2);
+        }
+};
