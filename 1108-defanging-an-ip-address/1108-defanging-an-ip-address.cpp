778e4abbd8f00c74f961d1893438f2bbfbfb8c81
Prince Sah <81484022+PrinceSah09@users.noreply.github.com>
2022-12-04 15:43:16 +0530

    Time: 0 ms (100.00%), Space: 6 MB (69.07%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string ans;
+        for(int i=0;i<address.length();i++)
+        {
+            if(address[i]=='.'){
+                ans.push_back('[');
+                ans.push_back('.');
+                ans.push_back(']');
+            }
+            else
+                ans.push_back(address[i]);
+        }
+        return ans;
+    }
+};
