ffd950322ac840eb550715f3fd2cbc73e668ce55
Prince Kumar <81484022+PrinceSah09@users.noreply.github.com>
2023-04-18 02:55:02 +0530

    Time: 155 ms (74.11%), Space: 85.4 MB (36.44%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -8,12 +8,33 @@ public:
         ans.push_back(root->val); // Pushing node value in vector
         solve(ans,root->right); // Right call
     }
+ 
+
 
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        vector<int>ans;
-        solve( ans, root1);
-        solve( ans, root2);
-        sort(ans.begin(),ans.end());
+        vector<int>nums1;
+        vector<int>nums2;
+        solve(nums1, root1);
+        solve(nums2, root2);
+        
+        vector<int>ans(nums1.size()+nums2.size());
+        int i=0,j=0;
+        int mainindex=0;
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i]<=nums2[j]){
+                ans[mainindex++] = nums1[i++];
+            }
+            else ans[mainindex++] = nums2[j++];
+        }
+        
+        while(i<nums1.size()){
+            ans[mainindex++] = nums1[i++];
+        }
+
+        while(j<nums2.size()){
+            ans[mainindex++] = nums2[j++];
+        }
         return ans;
+          
     }
 };
