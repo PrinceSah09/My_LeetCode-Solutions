fee50665b7e7623a9d4a8e11139542228a869edd
Prince Kumar <81484022+PrinceSah09@users.noreply.github.com>
2023-04-18 14:47:39 +0530

    Time: 58 ms (24.34%), Space: 41.4 MB (45.55%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
@@ -1,37 +1,21 @@
-
 class Solution {
 public:
+    int sum=0;
 
-    void dfs(TreeNode*root, int &sum){
-        if(root==NULL)return;
-
-        if(root->val%2==0){
-            //check the left-child of root is exist or not
-            if(root->left!=NULL){
-                if(root->left->left!=NULL){
-                    sum+=root->left->left->val;
-                }
-                if(root->left->right!=NULL){
-                    sum+=root->left->right->val;
-                }
-            }
-
-            //check the right-child of root is exist or not
-            if(root->right!=NULL){
-                if(root->right->left!=NULL){
-                    sum+=root->right->left->val;
-                }
-                if(root->right->right!=NULL){
-                    sum+=root->right->right->val;
-                }
-            }
+    void solve(TreeNode*root, TreeNode*Parents, TreeNode*grandParents){
+        if(root==nullptr)return;
+        
+        if(grandParents!=nullptr && grandParents->val%2==0){
+            sum+=root->val;
         }
-        dfs(root->left, sum);
-        dfs(root->right, sum);
+
+        //Recursive call
+        solve(root->left, root, Parents);
+        solve(root->right, root, Parents);
     }
+
     int sumEvenGrandparent(TreeNode* root) {
-        int sum=0;
-        dfs(root,sum);
+        solve(root, nullptr, nullptr);
         return sum;
     }
 };
