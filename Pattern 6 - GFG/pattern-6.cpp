8d29d471efc8a66d717ee9b61a2a7dd0d4036a17
Prince Kumar <81484022+PrinceSah09@users.noreply.github.com>
2023-06-22 11:44:15 +0530

    Added solution - LeetHub

--- Pattern 6 - GFG/pattern-6.cpp
+++ Pattern 6 - GFG/pattern-6.cpp
@@ -8,12 +8,22 @@ class Solution{
 public:
 	
 	void printTriangle(int n) {
-	     for(int i=1;i<=n;i++){
-	        for(int j=1;j<=n-i+1;j++){
-	            cout<<j<<" ";
-	        }
-	        cout<<endl;
-	    }
+    //Approach-2
+    for(int i=n;i>=1;i--){
+        for(int j=i;j>=1;j--){
+            cout<<i-j+1<<" ";
+        }
+        cout<<endl;
+    }
+	    
+	    
+	    //Approach-1
+	   //  for(int i=1;i<=n;i++){
+	   //     for(int j=1;j<=n-i+1;j++){
+	   //         cout<<j<<" ";
+	   //     }
+	   //     cout<<endl;
+	   // }
 	}
 };
 
