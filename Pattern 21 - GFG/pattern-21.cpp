e4f6ddb27c8328214fa4e67e5e06462ab98cdcc7
Prince Kumar <81484022+PrinceSah09@users.noreply.github.com>
2023-06-22 16:59:56 +0530

    Added solution - LeetHub

---
+++ Pattern 21 - GFG/pattern-21.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printSquare(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i==1 || i==n || j==1 || j==n){
+                    cout<<"*";
+                    
+                }
+                else cout<<" ";
+            }
+            cout<<endl;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printSquare(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
